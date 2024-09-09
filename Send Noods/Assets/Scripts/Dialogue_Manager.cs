3f5447f0b0d1727d9f8208d14aba06482e9e4277
zwan0195 <zwan0195@student.monash.edu>
2024-09-09 18:21:10 +1000

    Basic game
    basic game

---
+++ Send Noods/Assets/Scripts/Dialogue_Manager.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Dialogue_Manager : MonoBehaviour
+{
+    /// <summary>
+    /// dialogue text file
+    /// </summary>
+    public TextAsset dialogueDataFile;
+
+    /// <summary>
+    /// customer sprite
+    /// </summary>
+    public SpriteRenderer customerSprite;
+
+
+    /// <summary>
+    /// character name text
+    /// </summary>    
+    public TMP_Text nameText;
+
+    /// <summary>
+    /// dialogue text
+    /// </summary>
+    public TMP_Text dialogueText;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
