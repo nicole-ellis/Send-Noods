f398c08093270f1aa14065c21252fccf18ffb95f
Nicole Ellis <nell0012@student.monash.edu>
2024-10-28 21:18:41 +1100

    Finished Title Screen Lighting Animation
    Finalized code to have precise frame rate to match lighting aesthetic and to loop through each frame.

--- Send Noods/Assets/Scripts/TitleScreenAnimator.cs
+++ Send Noods/Assets/Scripts/TitleScreenAnimator.cs
@@ -6,14 +6,21 @@ public class TitleScreenAnimator : MonoBehaviour {
     [SerializeField] private Sprite[] frameArray;
     private int currentFrame;
     private float timer;
+    private float framerate = 1.0f;
+    private SpriteRenderer spriteRenderer;
+
+    private void Awake() {
+        spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+    }
+
 
     private void Update() {
         timer += Time.deltaTime;
 
-        if (timer >= 1f) {
-            timer-= 1f;
-            currentFrame++;
-            gameObject.GetComponent<SpriteRenderer>().sprite = frameArray[currentFrame];
+        if (timer >= framerate) {
+            timer -= framerate;
+            currentFrame = (currentFrame + 1) % frameArray.Length;
+            spriteRenderer.sprite = frameArray[currentFrame];
         }
     }
 
