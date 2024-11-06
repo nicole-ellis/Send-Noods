a17c670e6d78f08f2dbbb388b86194df084acd43
Aaron54301 <adme0004@student.monash.edu>
2024-11-07 10:31:18 +1100

    Reapply "Update animation"
    This reverts commit 66b82d782486aca4e1349e21ac7084427742ea70.

--- Send Noods/Assets/Scripts/change dialogue/BringChefBack.cs
+++ Send Noods/Assets/Scripts/change dialogue/BringChefBack.cs
@@ -9,6 +9,7 @@ public class BringChefBack : MonoBehaviour
     public Button mybutton;
     [SerializeField] private GameObject visualchef;
     public hideCustomer hideC;
+    public int FoodsComplete = 0;
 
 
     void Start()
@@ -26,6 +27,7 @@ public class BringChefBack : MonoBehaviour
 
     void OnButtonClick()
     {
+        FoodsComplete = FoodsComplete + 1;
         hideC.allowMoveChef = true;
         actualchef.GetComponent<SpriteRenderer>().enabled = true;
         actualchef.transform.position = visualchef.transform.position;
