d929b40bf9949636edcd147f9149c326e3afccb3
zwan0195 <zwan0195@student.monash.edu>
2024-11-10 22:38:43 +1100

    Update mini game + animation
    Update mini game + animation

--- Send Noods/Assets/Scripts/AnotherCustomerAnimation.cs
+++ Send Noods/Assets/Scripts/AnotherCustomerAnimation.cs
@@ -1,11 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Playables;
 
 public class AnotherCustomerAnimation : MonoBehaviour
 {
-    public Animator AnimateController;
-    public string AnimationName;
+    public PlayableDirector playableDirector;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,7 +13,7 @@ public class AnotherCustomerAnimation : MonoBehaviour
     }
 
     public void playAnimation(){
-        AnimateController.Play(AnimationName);
+        playableDirector.Play();
     }
 
     // Update is called once per frame
